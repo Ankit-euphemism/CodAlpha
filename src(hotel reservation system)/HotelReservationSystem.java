e9e27582a82772038f1322729762fe10dfe6172f
Ankit-euphemism <153090276+Ankit-euphemism@users.noreply.github.com>
2024-03-17 12:46:44 +0530

    Update HotelReservationSystem.java

--- src(hotel reservation system)/HotelReservationSystem.java
+++ src(hotel reservation system)/HotelReservationSystem.java
@@ -9,7 +9,7 @@ public class HotelReservationSystem extends JFrame implements ActionListener{
         
         public HotelReservationSystem() {
 		
-                setSize(1400,530); // setContentPane(300,300,1366,390);   frame size
+                setSize(2000,1000); // setContentPane(300,300,1366,390);   frame size
                 setLayout(null);
                 setLocation(0,20);
         
@@ -19,18 +19,16 @@ public class HotelReservationSystem extends JFrame implements ActionListener{
                 b1.setForeground(Color.BLACK);
 				
                 ImageIcon i1  = new ImageIcon(ClassLoader.getSystemResource("icons\\hotel.jpg"));
-                Image i3 = i1.getImage().getScaledInstance(1400, 530,Image.SCALE_DEFAULT);
+                Image i3 = i1.getImage().getScaledInstance(2000, 550,Image.SCALE_DEFAULT);
                 ImageIcon i2 = new ImageIcon(i3);
                 l1 = new JLabel(i2);
                 JLabel lid=new JLabel("HOTEL RESERVATION SYSTEM");
-                lid.setBounds(0,0,1400,50);
+                lid.setBounds(150,350,1200,50);
                 lid.setFont(new Font("serif",Font.PLAIN,60));
                 lid.setForeground(Color.black);
                 l1.add(lid);
-                
-                
                 b1.setBounds(1170,325,150,50);
-		l1.setBounds(0, 0, 1400, 530);
+		l1.setBounds(0, 50, 1400, 530);
                 
                 l1.add(b1);
 		add(l1);
@@ -39,19 +37,16 @@ public class HotelReservationSystem extends JFrame implements ActionListener{
                 
                 while(true){
                         lid.setVisible(false); // lid =  j label
-                    try{
-                        Thread.sleep(500); //1000 = 1 second
-                    }catch(Exception e){ 
-                        lid.setVisible(true);
-                    }
+                        try{
+                                Thread.sleep(500);//1000 =1second
+                        }
+                        catch(Exception e){ }
+                          lid.setVisible(true);
                     try{
                         Thread.sleep(500);
-                    }catch(Exception e){
-                        e.printStackTrace();
-                    }
+                    }catch(Exception e){} 
                 }
-	}
-        
+        }
         public void actionPerformed(ActionEvent ae){
                 new Login().setVisible(true);
                 this.setVisible(false);
